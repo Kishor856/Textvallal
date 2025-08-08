2792262bbef15e25b4180bd86413907f5f2a0c2c
Kishor856 <takkishor9784@gmail.com>
2025-08-08 19:37:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 class Config(object):
     # Telegram Bot ka token
-    BOT_TOKEN = "7691272055:AAEb2cq9_G3JKWrpq5NKSlXI6wEF1CLj4tc"
+    BOT_TOKEN = "7518659762:AAGIN4ulcTkdarlRN1ic9Ve8USK2FqujsPg"
     # Telegram API ki ID
-    API_ID = 27660379
-    API_HASH = "19c71c27733f0954371085198855125a"
+    API_ID = 24852999
+    API_HASH = "af27dc6de045fa54994453cb2d2316c1"
     ADMIN_ID = [5459854363]
     # MongoDB database ka URL
-    DB_URL = "mongodb+srv://sindhavravi11:%24Sind1234567890@cluster0.bwlgbx3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://takkishor9784:gG73juoh44MnvJEZ@cluster0.q8hxdk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Database ka naam
-    DB_NAME = "MY_BOT_DB"
+    DB_NAME = "lifegoal"
     # Text log channel ki ID
     TXT_LOG = -1002761572365
     # Authentication log channel ki ID
@@ -22,7 +22,7 @@ class Config(object):
     # Main channel ki ID
     CHANNEL = -1002761572365
     # Channel ka link
-    CH_URL = "https://t.me/ravi1996sindhav"
+    CH_URL = "https://t.me/+c72WBoMVF6I0ZTNl"
     # Bot ke owner ka Telegram link
     OWNER = "https://t.me/username"
     # Thumbnail image ka URL
